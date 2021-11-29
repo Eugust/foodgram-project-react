2b4b19a4b71caf60a265a593aebc03918bac74d8
Евгений <zhenya.hvan@gmail.com>
2021-11-28 20:54:36 -0800

    v1

---
+++ backend/api/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import (RecipeViewSet, TagViewSet, FavoriteRecipeViewSet,
+                    FollowViewSet, IngredientViewSet)
+
+router = SimpleRouter()
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'recipes', RecipeViewSet, basename='recipes')
+router.register(
+    r'recipes/(?P<recipe_id>\d+)/favorite',
+    FavoriteRecipeViewSet,
+    basename='favorite'
+)
+router.register(
+    r'users/subscriptions',
+    FollowViewSet,
+    basename='follow'
+)
+router.register(
+    r'ingredients',
+    IngredientViewSet,
+    basename='ingredients'
+)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import (RecipeSerializer, TagSerializer, FavoriteRecipeSerializer,
+                          FollowSerializer, IngredientSerializer)
+from recipes.models import Recipe, Tag, FavoriteRecipe, Follow, Ingredient
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class FavoriteRecipeViewSet(viewsets.ModelViewSet):
+    queryset = FavoriteRecipe.objects.all()
+    serializer_class = FavoriteRecipeSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
