e80d55e59b3d0c5553cd37682ed5e8f0818bc8ea
Евгений <zhenya.hvan@gmail.com>
2021-12-05 20:32:35 -0800

    pdf

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,10 @@
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.pagination import PageNumberPagination
@@ -17,6 +22,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(methods=['get'], detail=False, url_path='download_shopping_cart', url_name='download_shopping_cart')
+    def download_shopping_cart(self, request, *args, **kwargs):
+        buffer = io.BytesIO()
+
+        p = canvas.Canvas(buffer)
+
+        p.drawString(100, 100, "Hello world.")
+
+        p.showPage()
+        p.save()
+
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
 
 
 class TagViewSet(viewsets.ModelViewSet):
