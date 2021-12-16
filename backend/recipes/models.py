e967ddba308483fb8f828cdbb7691ff5482525d0
Евгений <zhenya.hvan@gmail.com>
2021-12-15 16:55:53 -0800

    Update models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-from colorfield.fields import ColorField
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
+
+from colorfield.fields import ColorField
 
 User = get_user_model()
 
