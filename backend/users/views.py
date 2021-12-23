e6a3b22b17c285ba8fb9d01ba43fa2f7d17405b0
Evgenii <zhenya.hvan@gmail.com>
2021-12-22 20:39:35 -0800

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -20,8 +20,7 @@ class UserViewSet(viewsets.ModelViewSet):
     pagination_class = PageNumberPagination
 
     @action(methods=['get'], detail=False,
-            url_path='me', url_name='me',
-            permission_classes=[IsAuthenticated])
+            url_path='me', url_name='me')
     def me(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             user = get_object_or_404(User, pk=self.request.user.id)
@@ -30,8 +29,7 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
     @action(methods=['post'], detail=False,
-            url_path='set_password', url_name='set_password',
-            permission_classes=[IsAuthenticated])
+            url_path='set_password', url_name='set_password')
     def set_password(self, request, *args, **kwargs):
         serializer = SetPasswordSerializer(data=self.request.data)
         new_password = self.request.data.get('new_password')
@@ -46,8 +44,7 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(methods=['get'], detail=False,
-            url_path='subscriptions', url_name='subscriptions',
-            permission_classes=[IsAuthenticated])
+            url_path='subscriptions', url_name='subscriptions')
     def subscriptions(self, request, *args, **kwargs):
         queryset = User.objects.filter(following__user=request.user).all()
 
@@ -67,8 +64,7 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(methods=['get', 'delete'], detail=False,
-            url_path=r'(?P<id>\d+)/subscribe',
-            permission_classes=[IsAuthenticated])
+            url_path=r'(?P<id>\d+)/subscribe')
     def subscribe(self, request, id=None, *args, **kwargs):
         user = get_object_or_404(User, id=request.user.id)
         following = get_object_or_404(User, id=self.kwargs.get('id'))
